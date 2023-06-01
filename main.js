ba44b246ee956e889d45e0f74d41c6ffda827aae
bruno <bruno@pop-os.localdomain>
2023-06-01 13:04:48 -0300

    Implementatio function call back for iteration

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
-function executeSoundPom() {
-    document.querySelector('#som_tecla_pom').play();
+function executeSound(idElementAudio) {
+    document.querySelector(idElementAudio).play();
 }
 
 const buttonList = document.querySelectorAll('.tecla');
+
+buttonList.forEach((button) => button.onclick = function () {
+    executeSound('#som_tecla_pom');
+});
 
 let name = buttonList[0].getAttributeNames();
 
